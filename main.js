68b514c247e5aef03839d22a1510863904cfef3f
MclPio <michaelpious@outlook.com>
2024-09-10 13:49:30 -0400

    Add function skeletons

--- main.js
+++ main.js
@@ -1,19 +1,27 @@
-function getLocationData(location) {
-  fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=${key}`,
+async function getWeatherData(location) {
+  const response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=WH4FQVH3ELQFFL6WF958XB45T`,
     {
       mode: "cors",
     }
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+  );
+  const weatherData = await response.json();
+  console.log(weatherData);
+  console.log(weatherData.resolvedAddress);
+  console.log(weatherData.days[0].temp);
+  console.log(weatherData.days[0].tempmax);
+  console.log(weatherData.days[0].tempmin);
+  console.log(weatherData.days[0].conditions);
+  console.log(weatherData.days[0].feelslike);
 }
+getWeatherData("toronto");
 
-getLocationData("toronto");
+//function to extract now data
+// => (temp, high, low, description, feels like)
+// chance to use a background image to describe condition...
+
+// function to extract daily data
+// => 10 day forecast (date, condition(where you use img), tempmax, tempmin)
+
+// function to extract hourly data
+// => 24 hour data (timeHour, temp, condition(where you use img))
