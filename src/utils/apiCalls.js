654a3f9a17de6f76675c42904594c993f6d59256
MclPio <113801014+MclPio@users.noreply.github.com>
2024-09-19 19:27:27 -0400

    Add api error handling

--- src/utils/apiCalls.js
+++ src/utils/apiCalls.js
@@ -1,4 +1,4 @@
-import weatherData from "../../sample.json";
+// import weatherData from "../../sample.json";
 import { updateSearchLoading } from "../ui/domManipulation";
 import {
   displayNowForecast,
@@ -6,21 +6,38 @@ import {
   displayDailyForecast,
 } from "../ui/domManipulation";
 
+const key = "WH4FQVH3ELQFFL6WF958XB45T";
+
 async function getWeatherData(location) {
-  // const response = await fetch(
-  //   `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=${key}`,
-  //   {
-  //     mode: "cors",
-  //   }
-  // );
-  // const weatherData = await response.json();
-  // need to handle erros or else loading bar stays on
-  console.log(weatherData);
-  displayNowForecast(weatherData);
-  displayHourlyForecast(weatherData);
-  displayDailyForecast(weatherData);
-  localStorage.setItem("location", weatherData.resolvedAddress);
-  updateSearchLoading();
+  try {
+    if (!location.trim()) {
+      throw new Error("location cannot be empty");
+    }
+
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=${key}`,
+      {
+        mode: "cors",
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const weatherData = await response.json();
+    if (weatherData.error) {
+      throw new Error(weatherData.error.message || "Unknown API error");
+    }
+    displayNowForecast(weatherData);
+    displayHourlyForecast(weatherData);
+    displayDailyForecast(weatherData);
+    localStorage.setItem("location", weatherData.resolvedAddress);
+  } catch (error) {
+    console.error("Error fetching weather data:", error.message);
+    alert(error.message);
+  } finally {
+    updateSearchLoading();
+  }
 }
 
 export { getWeatherData };
